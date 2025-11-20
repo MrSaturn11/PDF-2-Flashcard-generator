20099cef694803ec3bb9f7e3b47b38e9d6471d13
MrSaturn11 <122693130+MrSaturn11@users.noreply.github.com>
2025-11-20 10:21:29 +0000

    Update main.py

--- pdf2flashcard/main.py
+++ pdf2flashcard/main.py
@@ -1,9 +1,10 @@
 #read the README before trying to use
+#remember to paste your key into the key variable
 from pdf2image import convert_from_path
 import pytesseract
 from PIL import Image
 from google import genai
-key = "Your Key"
+key = "AIzaSyAm3pu3I1Yg8Ihu61k6zpPm5CheTYpveyQ"
 #pointing to where tesseract.exe is installed on my comptuer
 pytesseract.pytesseract.tesseract_cmd = r"C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
 pdf_name = input('what is your pdf file name?:')
@@ -26,11 +27,11 @@ with open("file.txt","r",encoding="utf-8") as f:
 client = genai.Client(api_key =key )
 
 response = client.models.generate_content(
-  model = "gemini-3-pro-preview",
+  model = "gemini-2.0-flash",
   contents=f"""
 you are a machine whos only purpose is to create high quality flash cards
 RULES FOR CREATING FLASHCARDS:
-1. answer should be no longer than 2 sentneces
+1. answer should be short and simple, one sentence max
 2. only exrract content that seems important
 CRITCAL
 only respond with plaintext in this format
